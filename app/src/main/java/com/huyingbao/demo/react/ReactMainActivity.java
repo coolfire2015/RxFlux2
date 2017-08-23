573ec31591a4579cfb5305cf648fd40c88b52dc3
K0170016-刘军锋 <k0170016@test.htsc.com.cn>
2017-08-23 14:32:25 +0800

    集成react native

---
+++ app/src/main/java/com/huyingbao/demo/react/ReactMainActivity.java
@@ -0,0 +1,21 @@
+package com.huyingbao.demo.react;
+
+import com.facebook.react.ReactActivity;
+
+/**
+ * Created by liujunfeng on 2017/8/23.
+ */
+
+public class ReactMainActivity extends ReactActivity {
+    /**
+     * 重写 getMainComponentName() 方法，返回的字符串必须和前面的
+     * AppRegistry.registerComponent('navigation', () => App) 里的 navigation 对应，
+     * 表示该 Activity 会显示对应组件里的内容。
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "navigation";
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native'
+import App from './js/App'
+//这里的 navigation 一般会根据模块功能命名，后面还会用到。
+AppRegistry.registerComponent('navigation', () => App)
