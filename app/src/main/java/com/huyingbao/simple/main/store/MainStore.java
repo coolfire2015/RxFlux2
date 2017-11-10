7ffeddb1e6af7ccce4249c200a7ca27c063ca782
K0170016 <K0170016@test.htsc.com.cn>
2017-11-10 11:09:24 +0800

    编写demo代码

--- app/src/main/java/com/huyingbao/simple/main/store/MainStore.java
+++ app/src/main/java/com/huyingbao/simple/main/store/MainStore.java
@@ -31,7 +31,7 @@ public class MainStore extends RxStore {
             case Actions.TO_GIT_REPO_LIST:
             case Actions.TO_GIT_USER:
                 break;
-            default:
+            default://此处不能省略，不是本模块的逻辑，直接返回，不发送RxStoreChange
                 return;
         }
         postChange(new RxStoreChange(getClass().getSimpleName(), rxAction));
